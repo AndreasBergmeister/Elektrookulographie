4ef7fc88961710404db6f46e1e72d2019c710f72
Andreas <andreas.bergmeister.ab@gmail.com>
2018-04-25 17:09:33 +0200

    Completed 'user'

--- file.py
+++ file.py
@@ -2,27 +2,25 @@ import json
 import time
 import os
 
-def save_signal(signal):
+def save_signal(signal, name):
     """
     Saves the signal as a json-string to a file
     Returns the filename
     """
-    filename = get_filename()
+    filename = get_filename(name)
     with open(filename, 'w') as file:
         json.dump(signal, file)
-    print('Saved signal to: ' + filename)
     return filename
 
 def get_signal(filename):
     with open(filename) as file:
         return json.load(file)
 
-def get_filename():
+def get_filename(name):
     """Loads the json-string from a file and retruns the object"""
     directory = 'records'
     filename = time.strftime("%Y%m%d-%H%M%S")
-    name = input('Input filename: ')
-    if(name):
+    if name:
         filename += '_' + name + '_'
     filename += '.json'
     return os.path.join(directory, filename)
--- user.py
+++ user.py
@@ -1,19 +1,47 @@
-import record
-import plot
+import argparse
+
+import record as r
+import plot as p
 import file
 
-print('Initializing')
 
-# Configure record
-duration = 0.5
-iterations = 1
-channels = 4
-# signal = record.record_advanced(duration, iterations, channels)
-signal = record.record()
+def record(args):
+    # Record Signal
+    if args.advanced:
+        signal = r.record_advanced(args.duration, args.iterations, args.channels)
+    else:
+        signal = r.record(args.channels)
+    print('Finished recording')
 
-print('Finished recording')
+    # Save Signal to file
+    filename = file.save_signal(signal, args.name)
+    print('Saved signal to: ' + filename)    
 
-# Save Signal
-file.save_signal(signal)
 
-# Plot Signal
+def plot(args):
+    signal = file.get_signal(args.name)
+    print('Signal loaded from file')
+    p.plot_channels(signal)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Record and plot signal from the OpenBCI Ganglion Board')
+    subparsers = parser.add_subparsers()
+
+    # Record Subparser
+    parser_record = subparsers.add_parser('record', help='Record the Signal and save it to a json-file')
+    parser_record.add_argument('-a', '--advanced', action='store_true')
+    parser_record.add_argument('-c', '--channels', type=int, default=4)
+    parser_record.add_argument('-d', '--duration', type=float, default=1)
+    parser_record.add_argument('-i', '--iterations', type=int, default=1)
+    parser_record.add_argument('-n', '--name', help='Filename of the json-file')
+    parser_record.set_defaults(func=record)
+
+    # Plot Subparser
+    parser_print = subparsers.add_parser('plot', help='Plots the Signal of a file')
+    parser_print.add_argument('name', help='Filename')
+    parser_print.set_defaults(func=plot)
+    
+   
+    args = parser.parse_args()
+    args.func(args)
